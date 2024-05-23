c6f084268a458f0e28f2a17c0399f3b77324d220
mohsenzekavat <mohsenzekavat84@gmail.com>
2024-05-23 23:04:23 +0330

    Created Task.py and ImportantFunctions.py
    Task.py contains the classes needed for tasks and ImportantFunctions.py contains all the functions in SignUp&Login plus make task and project which are in complete.

--- ImportantFunctions.py
+++ ImportantFunctions.py
@@ -1,8 +1,12 @@
 import User
 import UserInfo
+import project
+import Task
 import time
 import os
 import re
+import uuid
+from datetime import datetime
 
 
 def sign_up():
@@ -60,6 +64,73 @@ def sign_up():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def login():
+    print("Hello and welcome to our program! Here you can login into your account. Please fill out the information carefully.")
+    time.sleep(5)
+    list_data = UserInfo.read_user_info()
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        username = input("Please enter your username: ")
+        password = input("Please enter your password: ")
+        if not valid_info(username, password, list_data):
+            print("Login was unsuccessful! Wrong username or password.")
+            time.sleep(3)
+        else:
+            print("Login was successful!")
+            time.sleep(3)
+            break
+
+
+def create_project():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Here you can create a project to your liking. Answer the questions carefully.")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    title = input("Enter the title for the project: ")
+    random_id = str(uuid.uuid4())
+
+
+def create_task():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    list_data = UserInfo.read_user_info()
+    print("In this part you can create a task and assign it to a member of your project.")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    title = input("Enter a title for your task: ")
+    identification_code = str(uuid.uuid4())
+    info = input("You can write the details and important things about this task over here: ")
+    start = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    information = input("Enter the username or email of the people you want to assign this task to with a comma between their information: ").split(',')
+    assigned_to = []
+    for i in information:
+        check = re.match(pattern, i)
+        if check:
+            assigned_to.append(find_user(i, list_data, 1))
+        else:
+            assigned_to.append(find_user(info, list_data, 0))
+    priority_check = input("Choose the priority of this task: LOW, MEDIUM, HIGH, CRITICAL: ")
+    if priority_check == "LOW":
+        priority = Task.Priority.LOW
+    elif priority_check == "MEDIUM":
+        priority_check = Task.Priority.MEDIUM
+    elif priority_check == "HIGH":
+        priority = Task.Priority.HIGH
+    elif priority_check == "CRITICAL":
+        priority = Task.Priority.CRITICAL
+    status_check = input("Choose the status of this task: BACKLOG, TODO, DOING, DONE, ARCHIVED: ")
+    if status_check == "BACKLOG":
+        status = Task.Status.BACKLOG
+    elif status_check == "TODO":
+        status = Task.Status.TODO
+    elif status_check == "DOING":
+        status = Task.Status.DOING
+    elif status_check == "DONE":
+        status = Task.Status.DONE
+    elif status_check == "ARCHIVED":
+        status = Task.Status.ARCHIVED
+
+
 def valid_username(username, data):
     for i in data:
         if username == i["username"]:
@@ -90,23 +161,6 @@ def valid_email(email, data):
     return 0
 
 
-def login():
-    print("Hello and welcome to our program! Here you can login into your account. Please fill out the information carefully.")
-    time.sleep(5)
-    list_data = UserInfo.read_user_info()
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        username = input("Please enter your username: ")
-        password = input("Please enter your password: ")
-        if not valid_info(username, password, list_data):
-            print("Login was unsuccessful! Wrong username or password.")
-            time.sleep(3)
-        else:
-            print("Login was successful!")
-            time.sleep(3)
-            break
-
-
 def valid_info(username, password, data):
     for i in data:
         if username == i["username"]:
@@ -115,3 +169,14 @@ def valid_info(username, password, data):
                     return True
             return False
     return False
+
+
+def find_user(info, data, check):
+    if check == 0:
+        for i in data:
+            if info == i["username"]:
+                return i
+    else:
+        for i in data:
+            if info == i["email"]:
+                return i
---
+++ Task.py
@@ -0,0 +1,116 @@
+# Task.py
+# Status
+# Priority
+from enum import Enum
+import string
+
+
+class Priority:
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+    CRITICAL = 4
+
+
+class Status:
+    BACKLOG = 1
+    TODO = 2
+    DOING = 3
+    DONE = 4
+    ARCHIVED = 5
+
+
+class Task:
+    def __init__(self, identification_code, title, info, start, end, assigned_to, priority: Priority, status: Status, history, comments):
+        self.identification_code = identification_code
+        self.title = title
+        self.info = info
+        self.start = start
+        self.end = end
+        self.assigned_to = assigned_to
+        self.priority = priority
+        self.status = status
+        self.history = history
+        self.comments = comments
+
+    def set_identification_code(self, identification_code):
+        self.identification_code = identification_code
+
+    def get_identification_code(self):
+        return self.identification_code
+
+    def set_name(self, title):
+        self.title = title
+
+    def get_name(self):
+        return self.title
+
+    def set_info(self, info):
+        self.info = info
+
+    def get_info(self):
+        return self.info
+
+    def set_start(self, start):
+        self.start = start
+
+    def get_start(self):
+        return self.start
+
+    def set_end(self, end):
+        self.end = end
+
+    def get_end(self):
+        return self.end
+
+    def set_assigned_to(self, assigned_to):
+        self.assigned_to = assigned_to
+
+    def get_assigned_to(self):
+        return self.assigned_to
+
+    def set_priority(self, priority):
+        self.priority = priority
+
+    def get_priority(self):
+        return self.priority
+
+    def set_status(self, status):
+        self.status = status
+
+    def get_status(self):
+        return self.status
+
+    def set_history(self, history):
+        self.history = history
+
+    def get_history(self):
+        return self.history
+
+    def set_comments(self, comments):
+        self.comments = comments
+
+    def get_comments(self):
+        return self.comments
+
+
+# Create a task
+my_task = Task(
+    identification_code=1,
+    title="Sample Task",
+    info="Some task information",
+    start="2024-05-22",
+    end="2024-05-30",
+    assigned_to="John Doe",
+    priority=Priority.HIGH,
+    status=Status.TODO,
+    history=[],
+    comments=[]
+)
+
+# Print task details
+# print(my_task.get_name())
+# print(my_task.get_priority())
+# print(my_task.get_status())
+# print(my_task.get_priority())
+print(Priority.HIGH)
